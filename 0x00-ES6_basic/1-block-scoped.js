5499bc8301d92470d31963112c714db790109c65
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-10-01 01:15:12 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,24 +1,13 @@
-// 1-block-scoped.js
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
-  /* Declaring task with const because the value doesn't need to change */
-  const task = false;
+  var task = false;
+  var task2 = true;
 
-  /* Declaring task2 with const for the same reason as task */
-  const task2 = true;
-
-  /* Checking the value of trueOrFalse */
   if (trueOrFalse) {
-    /* Using let for task to ensure it's block-scoped and doesn't overwrite the outer task */
-    let task = true;
-
-    /* Using let for task2 to ensure it's block-scoped and doesn't overwrite the outer task2 */
-    let task2 = false;
-
-    /* Avoiding unused block-scoped variables */
-    console.log(task, task2); // Log the block-scoped variables to prevent unused errors
+    const task = true;
+    const task2 = false;
   }
 
-  /* Returning the outer task and task2 values */
   return [task, task2];
 }
