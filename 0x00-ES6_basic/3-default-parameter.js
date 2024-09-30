4eaf01f6d35c0f4ae0ed8aff54a14723def0fcf2
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-10-01 01:18:20 +0300

    Task 3 Parameter defaults

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,4 @@
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  /* Returning the sum of initialNumber, expansion1989, and expansion2019 */
+  return initialNumber + expansion1989 + expansion2019;
+}
