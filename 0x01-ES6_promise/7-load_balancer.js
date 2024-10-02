f694542687cc09719a67a72cfbb33a16202e70a9
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-10-02 22:54:42 +0300

    Test 7
    Load balancer

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,6 @@
+/* Define and export the loadBalancer function */
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise
+    .race([chinaDownload, USDownload])
+    .then((res) => res);
+}
