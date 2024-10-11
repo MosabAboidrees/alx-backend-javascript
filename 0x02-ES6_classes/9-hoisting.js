0b3dec5818cc7e8d4673f410f8702d3d7b5ee2ac
MosabAboidrees <mosab_mahala@yahoo.com>
2024-10-11 20:38:25 +0300

    Task 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -37,7 +37,8 @@ export class StudentHolberton {
 
   // Getter for fullStudentDescription
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - \
+${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
@@ -52,5 +53,7 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
+const listOfStudents = [student1, student2, student3, student4, student5];
+
 // Export the list of students
-export const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
