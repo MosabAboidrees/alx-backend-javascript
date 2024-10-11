90c4a1a158487adb0632c467384f63a579179d41
MosabAboidrees <mosab_mahala@yahoo.com>
2024-10-11 19:30:37 +0300

    Task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+// 0-classroom.js
+
+// Define the ClassRoom class
+export default class ClassRoom {
+// Constructor with maxStudentsSize parameter
+  constructor(maxStudentsSize) {
+  // Assign maxStudentsSize to the private attribute _maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
