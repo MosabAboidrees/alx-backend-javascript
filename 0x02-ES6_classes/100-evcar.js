02f648ae90ce5df7fb9b61f1cd6b8f82df89892a
MosabAboidrees <mosab_mahala@yahoo.com>
2024-10-11 20:38:48 +0300

    Task 11

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+// Import the Car class
+import Car from './10-car.js';
+
+// Define the EVCar class that extends Car
+export default class EVCar extends Car {
+  // Constructor with parameters for brand, motor, color, and range
+  constructor(brand, motor, color, range) {
+    // Call the parent constructor for brand, motor, and color
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // Override the cloneCar method to return an instance of Car
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
