fce06676413ff34bc9b8ddf8fd262ae8b969c0b8
MosabAboidrees <mosab_mahala@yahoo.com>
2024-11-30 00:04:44 +0300

    Task 0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * Prints a message to the standard output.
+ * @param {string} message - The message to be displayed.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
