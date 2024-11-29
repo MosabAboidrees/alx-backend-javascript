9dcdf247e2a08fb2b3d8ed3aa934dfeae06ffaff
MosabAboidrees <mosab_mahala@yahoo.com>
2024-11-30 00:13:21 +0300

    Task 1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,11 +6,9 @@ console.log('Welcome to Holberton School, what is your name?');
 // Set up the stdin stream to read user input
 process.stdin.on('data', (data) => {
   const name = data.toString().trim(); // Get the user input and remove trailing spaces
-  console.log(`Your name is: ${name}`); // Print the user's name
+  console.log(`Your name is: ${name}`);
+  if (!process.stdin.isTTY) { // Check if input is piped
+    console.log('This important software is now closing');
+  }
   process.exit(); // End the process
-});
-
-// Handle the program's exit event
-process.on('exit', () => {
-  console.log('This important software is now closing');
 });
